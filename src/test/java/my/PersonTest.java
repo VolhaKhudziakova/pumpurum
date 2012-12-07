c0ce20e4066d7bdc3e12e411d9700f824be9cae9
VolhaKhudziakova <volha-khudziakova@epam.com>
2012-12-07 16:30:06 +0300

    commit!

---
+++ src/test/java/my/PersonTest.java
@@ -0,0 +1,12 @@
+package my;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PersonTest {
+    @Test
+    public void canConstructAPersonWithAName() {
+        Person person = new Person("Larry");
+        assertEquals("Larry", person.getName());
+    }
+}
